4c11ee823f0e633c6e2926bfab3d604650f273b2
Romell Pineda <romell.pineda@gmail.com>
2020-01-28 10:30:56 -0800

    first commit

---
+++ src/test/java/io/romellpineda/codefellowship/CodefellowshipApplicationTests.java
@@ -0,0 +1,13 @@
+package io.romellpineda.codefellowship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodefellowshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
